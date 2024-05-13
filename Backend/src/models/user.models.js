6fb853a32b462b124ab8aea68a5b7ba6b15d6027
Ravinesh Mishra <119425262+Ravinesh18@users.noreply.github.com>
2024-05-13 11:10:41 +0530

    user model changes

--- Backend/src/models/user.models.js
+++ Backend/src/models/user.models.js
@@ -18,13 +18,7 @@ const userSchema  = new Schema({
         trim : true ,
 
     },
-    firstName:{
-        type: String ,
-        required : true ,
-      
-
-    },
-    lastName:{
+    fullname:{
         type: String ,
         required : true ,
     },
@@ -58,6 +52,44 @@ const userSchema  = new Schema({
 {
     timestamps:true
 })
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 
 
